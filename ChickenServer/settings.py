d0cfe8ee89168d5b37c66db14ed8266e7b4ca24e
superman <superman@bitave.com>
2012-11-14 00:14:28 +0900

    Implemented Model!

--- ChickenServer/settings.py
+++ ChickenServer/settings.py
@@ -132,7 +132,7 @@ INSTALLED_APPS = (
     'Stores',
     
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- Stores/models.py
+++ Stores/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+
+class Store(models.Model):
+
+    name = models.CharField(max_length=50, null=False)
+    # store name
+
+    category = models.CharField(max_length=20, null=True, blank=True)
+    # category ( meal / alcohol)
+    locationX = models.FloatField(null=False, default=0.0, blank=True)
+    locationY = models.FloatField(null=False, default=0.0, blank=True)
+    # location (GPS)
+    address = models.CharField(max_length=150, null=False)
+    # address
+    phone = models.CharField(max_length=25, null=False)
+    # phone
+    main_image = models.ImageField(upload_to="main_image", null=True, blank=True)
+    # image
+    open_at = models.PositiveSmallIntegerField(null=True, blank=True)
+    # business hour from
+    close_at = models.PositiveSmallIntegerField(null=True, blank=True)
+    # business hour to
+    max_seats = models.IntegerField(null=False, default=0)
+    # max capacity
+    available_seats = models.IntegerField(null=True)
+    # (+) available seats
+    registered_at = models.DateTimeField(auto_now_add=True)
+    # registered date (auto)
+
+    def __unicode__ (self):
+        return self.name
+    
+class Review(models.Model):
+    store = models.ForeignKey(Store, null=False)
+    # foreign key pointing store
+    saved_at = models.DateTimeField(auto_now_add=True)
+    # date
+    score = models.PositiveSmallIntegerField(null=True, default=0)
+    # score
+    comment = models.TextField(null=False)
+    # comment (review)
+    review_image = models.ImageField(upload_to="review_image", null=True, blank=True)
+    # image
+
+    def __unicode__ (self):
+        return self.comment
+
+class Picture(models.Model):
+    store = models.ForeignKey(Store, null=False)
+    # foreign key pointing store
+    store_image = models.ImageField(upload_to="store_image", null=False)
+    # image
+
+class Menu(models.Model):
+    store = models.ForeignKey(Store, null=False)
+    # foreign key pointing store
+    name = models.CharField(max_length=50, null=False)
+    # menu name
+    price = models.IntegerField(null=True, blank=True)
+    # price
+    menu_image = models.ImageField(upload_to="menu_image", null=True, blank=True)
+    # image
+    
+    def __unicode__ (self):
+        return self.name
+    
