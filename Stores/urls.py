2bce59e67bffc01dc839d99315b61fd8cf5bb259
superman <superman@bitave.com>
2012-11-14 02:54:40 +0900

    store view

---
+++ Stores/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+import Stores.views
+
+urlpatterns = patterns('',
+    # Examples:
+    # Uncomment the next line to enable the admin:
+    url(r'^store_list/$', 'Stores.views.store_list'),
+    url(r'^store_view/(?P<store_id>\d+)/$', 'Stores.views.store_view'),
+    url(r'^store_register/$', 'Stores.views.store_register'),
+    url(r'^review_register/(?P<store_id>\d+)/$', 'Stores.views.comment_register'),
+)
