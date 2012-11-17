c98fe15b2dc6c1be6a5b86e5599a8211190611b3
superman <superman@bitave.com>
2012-11-18 02:25:02 +0900

    bug fixed

--- ChickenServer/settings.deploy.py
+++ ChickenServer/settings.deploy.py
@@ -50,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = "/var/www/ChickenServer/static"
+MEDIA_ROOT = "/var/www/static"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
