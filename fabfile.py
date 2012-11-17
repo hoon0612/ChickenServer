124037f75581e34d11bf8866889a459261ab3d84
superman <superman@bitave.com>
2012-11-18 00:36:43 +0900

    bug fixed

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,8 @@ from fabric.api import run, local
 
 def deploy():
     local('git push')
-    run('cd /var/www/ChickenServer/ git pull git://github.com/hoon0612/ChickenServer.git')
+    run('cd /var/www/ChickenServer.git/; git pull git://github.com/hoon0612/ChickenServer.git')
+    run('rm -rf /var/www/ChickenServer')
+    run('cp -R /var/www/ChickenServer.git /var/www/ChickenServer')
     run('mv /var/www/ChickenServer/ChickenServer/settings.deploy.py /var/www/ChickenServer/ChickenServer/settings.py')
     run('service apache2 restart')
